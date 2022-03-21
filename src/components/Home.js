37af15cd9d753c80056882823c39f2b816aa2d62
chandu <chanduyadav1515@gmail.com>
2022-03-21 17:03:32 +0530

    Home UI Added and displaying data by getting from firestore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { firestore } from '../Firestore';
+import {Link} from 'react-router-dom'
+function Home() {
+    const [posts,setPosts] = useState([]);
+   
+    useEffect(()=>{
+        firestore.collection('blog-content').get().then((snapShot)=>{
+            const posts = snapShot.docs.map((doc)=>{
+                return{
+                    id:doc.id,
+                    ...doc.data(),
+                }
+            })
+            setPosts(posts)
+            
+        })
 
-function Home(props) {
+    },[])
+
     return (
-        <div>
-            Home
-        </div>
+        <div className="home">
+        <h1>All Blogs</h1>
+        <div id="blog-by">Chandu</div>
+  
+        {posts.map((post, index) => (
+          <div className="post" key={`post-${index}`}>
+            <Link to={`/post/${post.id}`}>
+              <h3>{post.title}</h3>
+            </Link>
+  
+            <p>{post.subTitle}</p>
+          </div>
+        ))}
+      </div>
     );
 }
 
