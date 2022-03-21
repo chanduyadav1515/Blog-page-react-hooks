8fd092a2f67fb77bc2ebfaf863ae8f28f53cb2fe
chandu <chanduyadav1515@gmail.com>
2022-03-21 14:23:34 +0530

    added firestore

---
+++ src/components/firestore.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmc8Jqey-qbR2FtZZwr1L_sXNscDMHcAg",
+  authDomain: "building-blog-page.firebaseapp.com",
+  projectId: "building-blog-page",
+  storageBucket: "building-blog-page.appspot.com",
+  messagingSenderId: "260432616630",
+  appId: "1:260432616630:web:7562b480012d777bd9da17"
+};
+
+// Initialize Firebase
+const Db = firebase.initializeApp(firebaseConfig);
+
+export default Db;
